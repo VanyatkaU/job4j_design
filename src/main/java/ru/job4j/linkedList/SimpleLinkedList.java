d2612b824f68be1438e7a8451d6e0ba55396a142
VanyatkaU <ivanKa49@ya.ru>
2022-02-04 13:26:13 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/linkedList/SimpleLinkedList.java
@@ -0,0 +1,77 @@
+package ru.job4j.linkedList;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+    private int modCount;
+
+    public static class Node<E> {
+
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> l = first;
+        for (int i = 0; i < index; i++) {
+            l = l.next;
+        }
+        return l.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            final int expectedModCount = modCount;
+            private Node<E> value = first;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return value != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E item = value.item;
+                value = value.next;
+                return item;
+            }
+        };
+    }
+}
