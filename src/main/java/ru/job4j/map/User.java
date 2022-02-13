d0b175f7edf32c300e4a6057dfa8fb0d0c28572e
VanyatkaU <ivanKa49@ya.ru>
2022-02-13 17:55:49 +0300

    4. Переопределить только equals [#1004 #264156]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -15,17 +15,27 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
     }
 
     public static void main(String[] args) {
-        Calendar data = new GregorianCalendar(2013, 12, 13);
-        User son = new User("Stepan", 1, data);
-        User son1 = new User("Stepan", 1, data);
+        Calendar dataV = new GregorianCalendar(2013, 12, 13);
+        Calendar dataS = new GregorianCalendar(2020, 07, 07);
+        User daughter = new User("Vasilina", 1, dataV);
+        User son = new User("Stepan", 2, dataS);
         Map<User, Object> map = new HashMap<>();
+        map.put(daughter, new Object());
         map.put(son, new Object());
-        map.put(son1, new Object());
         System.out.println(map);
     }
 }
