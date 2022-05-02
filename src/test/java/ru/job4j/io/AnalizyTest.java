560086fea7517b163070cf7a71d4c76fcafc563e
VanyatkaU <ivanKa49@ya.ru>
2022-05-02 16:44:58 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -34,26 +34,4 @@ public class AnalizyTest {
         }
         assertThat(rsl.toString(), is("10:58:01;10:59:01" + "11:01:02;11:02:02"));
     }
-
-    @Test
-    public void unavailable1() throws IOException {
-        File source = folder.newFile("server.log");
-        File target = folder.newFile("unavailable.csv");
-        try (PrintWriter out = new PrintWriter(source)) {
-            out.println("""
-                    200 10:56:01\r
-                    500 10:57:01\r
-                    400 10:58:01\r
-                    500 10:59:01\r
-                    400 11:01:02\r
-                    200 11:02:02""");
-        }
-        Analizy analizy = new Analizy();
-        analizy.unavailable(source.getAbsolutePath(),target.getAbsolutePath());
-        StringBuilder rsl = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            in.lines().forEach(rsl::append);
-        }
-        assertThat(rsl.toString(), is("10:57:01;11:02:02"));
-    }
 }
