b6b66ffe1ff8700658d86b81febde6391bb07e9d
VanyatkaU <ivanKa49@ya.ru>
2022-05-17 20:20:28 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,14 +10,31 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".txt"))
-                .forEach(System.out::println);
+        if (isValid(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, path -> path.toFile().getName()
+                    .endsWith(args[1]))
+                    .forEach(System.out::println);
+        }
     }
 
     private static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static boolean isValid(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("The root folder is empty or there are not enough options. "
+                                               + "Use template: root folder, .txt");
+        }
+        if (args.length > 2) {
+            throw new IllegalArgumentException("More than two arguments. Specify the number of arguments - 2");
+        }
+        if (!new File(args[0]).isDirectory()) {
+            throw new IllegalArgumentException("Not directory.");
+        }
+        return true;
     }
 }
