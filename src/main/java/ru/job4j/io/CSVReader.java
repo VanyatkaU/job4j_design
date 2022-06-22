755bc49199e98d3339134a3c1470cd29d142698f
VanyatkaU <ivanKa49@ya.ru>
2022-06-22 21:50:04 +0300

    7. Scanner [#504791 #312067]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -58,8 +58,8 @@ public class CSVReader {
                     pw.println(result);
                 } catch (IOException e) {
                     e.printStackTrace();
-                    result = new StringJoiner(argsName.get("delimiter"));
                 }
+                result = new StringJoiner(argsName.get("delimiter"));
             }
         }
     }
