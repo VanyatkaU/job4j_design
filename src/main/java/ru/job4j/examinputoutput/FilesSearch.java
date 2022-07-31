4236ac19ac022ba2e706604e0ef97148defcf338
VanyatkaU <ivanKa49@ya.ru>
2022-07-31 15:00:39 +0300

    2. Поиск файлов по критерию [#783]

--- src/main/java/ru/job4j/examinputoutput/FilesSearch.java
+++ src/main/java/ru/job4j/examinputoutput/FilesSearch.java
@@ -31,13 +31,12 @@ public class FilesSearch {
     }
 
     private static void isValid(String[] args, ArgsName argsName) {
-        String help = """
-                Программа должна собираться в jar и запускаться через java -jar find.jar -d=c:/ -n=*.?xt -t=mask -o=log.txt
-                Ключи:
-                -d - директория, в которой начинать поиск.
-                -n - имя файла, маска, либо регулярное выражение.
-                -t - тип поиска: mask искать по маске, name по полному совпадению имени, regex по регулярному выражению.
-                -o - результат записать в файл .txt.""";
+        String help = "Программа должна собираться в jar и запускаться через java -jar find.jar -d=c:/ -n=*.?xt -t=mask -o=log.txt"
+                + "Ключи:"
+                + "-d - директория, в которой начинать поиск."
+                + "-n - имя файла, маска, либо регулярное выражение."
+                + "-t - тип поиска: mask искать по маске, name по полному совпадению имени, regex по регулярному выражению."
+                + "-o - результат записать в файл .txt.";
         if (args.length != 4) {
             throw new IllegalArgumentException("The root folder is empty or "
                                                + "it does not have the correct number of arguments. "
