351dd8abd67a2c9dca3517f08400f41a75f0dc7f
VanyatkaU <ivanKa49@ya.ru>
2022-08-01 19:31:53 +0300

    2. Поиск файлов по критерию [#783 #327391]

--- src/main/java/ru/job4j/examinputoutput/FilesSearch.java
+++ src/main/java/ru/job4j/examinputoutput/FilesSearch.java
@@ -1,30 +1,42 @@
 package ru.job4j.examinputoutput;
 
 import ru.job4j.io.ArgsName;
+import ru.job4j.io.SearchFiles;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class FilesSearch {
 
-    private static void findFiles(ArgsName argsName) {
+    private static Predicate<Path> typeSelection(ArgsName argsName) {
         Predicate<Path> result = null;
         if ("name".equals(argsName.get("t"))) {
             result = path -> path.toFile().getName().equals(argsName.get("n"));
         } else if ("mask".equals(argsName.get("t"))) {
-            result = path -> path.toFile().getName().equals(argsName.get("n"));
+            result = path -> path.toFile().getName().matches(argsName.get("n")
+                    .replace("*", ".*"));
         } else if ("regex".equals(argsName.get("t"))) {
             result = path -> path.toFile().getName().equals(argsName.get("n"));
         }
-        writeToFile(argsName, result);
+        return result;
     }
 
-    private static void writeToFile(ArgsName argsName, Predicate<Path> result) {
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    private static void writeToFile(List<Path> files, String result) {
         try (PrintWriter writer = new PrintWriter(
-                new FileWriter(argsName.get("o"), StandardCharsets.UTF_8))) {
-            writer.println(result);
+                new FileWriter(result, StandardCharsets.UTF_8))) {
+            for (Path file : files) {
+                writer.println(file);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,11 +44,11 @@ public class FilesSearch {
 
     private static void isValid(String[] args, ArgsName argsName) {
         String help = "Программа должна собираться в jar и запускаться через java -jar find.jar -d=c:/ -n=*.?xt -t=mask -o=log.txt"
-                + "Ключи:"
-                + "-d - директория, в которой начинать поиск."
-                + "-n - имя файла, маска, либо регулярное выражение."
-                + "-t - тип поиска: mask искать по маске, name по полному совпадению имени, regex по регулярному выражению."
-                + "-o - результат записать в файл .txt.";
+                      + "Ключи:"
+                      + "-d - директория, в которой начинать поиск."
+                      + "-n - имя файла, маска, либо регулярное выражение."
+                      + "-t - тип поиска: mask искать по маске, name по полному совпадению имени, regex по регулярному выражению."
+                      + "-o - результат записать в файл .txt.";
         if (args.length != 4) {
             throw new IllegalArgumentException("The root folder is empty or "
                                                + "it does not have the correct number of arguments. "
@@ -55,9 +67,11 @@ public class FilesSearch {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(args);
         isValid(args, argsName);
-        findFiles(argsName);
+        Predicate<Path> result = typeSelection(argsName);
+        List<Path> list = search(Path.of(argsName.get("d")), result);
+        writeToFile(list, argsName.get("o"));
     }
 }
