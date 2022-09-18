db3251c8f67c9b425609a55c1efca4d6ca20d042
Ivan Karelkin <ivanKa49@ya.ru>
2022-09-18 10:25:41 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Disabled
+class GeneratorTest {
+
+    @Test
+    void produce() {
+        Generator generator = new SimpleGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("subject", "you");
+        assertThat(generator.produce("I am ${name}, Who are ${subject}", map))
+                .isEqualTo("I am a Ivan, Who are you");
+    }
+
+    @Test
+    void whenIncorrectKey() {
+        Generator generator = new SimpleGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("surname", "Ivan");
+        map.put("subject", "you");
+        assertThrows(IllegalArgumentException.class, () ->
+            generator.produce("I am ${name}, Who are ${subject}", map)
+        );
+    }
+
+    @Test
+    void whenTooMoreKeys() {
+        Generator generator = new SimpleGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("surname", "Karelkin");
+        map.put("subject", "you");
+        assertThrows(IllegalArgumentException.class, () ->
+                generator.produce("I am ${name}, Who are ${subject}", map)
+        );
+    }
+
+    @Test
+    void whenIncorrectMap() {
+        Generator generator = new SimpleGenerator();
+        Map<String, String> map = new HashMap<>();
+        assertThrows(IllegalArgumentException.class, () ->
+                generator.produce("I am ${name}, Who are ${subject}", map)
+        );
+    }
+
+}
