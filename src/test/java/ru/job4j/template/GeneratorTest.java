cd2e5207455a09a70ef41c0f8fdeaf8c3d702205
Ivan Karelkin <ivanKa49@ya.ru>
2022-09-18 14:13:24 +0300

    3. Шаблонизатор. [#855 #344604]

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class GeneratorTest {
 
     @Test
-    void produce() {
+    void whenCorrectProduce() {
         Generator generator = new SimpleGenerator();
         Map<String, String> map = new HashMap<>();
         map.put("name", "Ivan");
