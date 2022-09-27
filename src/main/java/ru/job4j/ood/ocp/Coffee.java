63957e8a8af934659e0e4efce6d26627ad0cca2f
Ivan Karelkin <ivanKa49@ya.ru>
2022-09-27 21:03:56 +0300

    0. Принцип открытости закрытости [#4914 #346128]

--- src/main/java/ru/job4j/ood/ocp/Coffee.java
+++ src/main/java/ru/job4j/ood/ocp/Coffee.java
@@ -1,33 +1,25 @@
 package ru.job4j.ood.ocp;
 
+import java.util.List;
+
 /**
  * Класс описывает кофейные напитки из разных сортов кофе.
- * Растет популярность чаепития и нам нужно добавить чай.
- * Необходимо будет изменить класс, добавить классы чая,
- * что нарушает принцип ОСР.
- * Просто расширить класс Coffee нельзя. Решение - создать
- * интерфейс Drinks и от него реализовывать напитки по классам.
+ * Поле coffees должно быть типом абстракции, но на данный момент
+ * является списком типа Coffee.
+ * Метод addForCooking() принимает параметр конкретного типа.
+ * При добавлении для приготовления другого напитка, необходимо
+ * будет вносить изменения в класс.
  */
 
-public class Coffee {
+public class Coffee implements Drinks {
 
-    private static class Arabic extends Coffee {
-        public void espresso() {
+    private List<Coffee> coffees;
 
-        }
+    private class Arabic extends Coffee {
 
-        public void americano() {
-
-        }
-    }
-
-    private static class Robust extends Coffee {
-        public void cappuccino() {
-
-        }
-
-        public void latte() {
-
+        public Coffee addForCooking(Coffee espresso) {
+            coffees.add(espresso);
+            return espresso;
         }
     }
 }
