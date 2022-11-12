7ad0055275fa427b72818e70eefd934e83c74ee3
Ivan Karelkin <ivanKa49@ya.ru>
2022-11-12 16:37:41 +0300

    2. Парковка машин [#853 #357349]

--- src/test/java/ru/job4j/ood/lsp/parking/placement/AbstractParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/placement/AbstractParkingTest.java
@@ -5,6 +5,7 @@ import ru.job4j.ood.lsp.parking.model.Car;
 import ru.job4j.ood.lsp.parking.model.Passenger;
 import ru.job4j.ood.lsp.parking.model.Truck;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,9 +17,9 @@ class AbstractParkingTest {
         Car passengerOne = new Passenger();
         Car passengerTwo = new Passenger();
         Car truckOne = new Truck(2);
-        assertTrue(parking.add(passengerOne));
-        assertTrue(parking.add(passengerTwo));
-        assertTrue(parking.add(truckOne));
+        assertThat(parking.add(passengerOne)).isTrue();
+        assertThat(parking.add(passengerTwo)).isTrue();
+        assertThat(parking.add(truckOne)).isTrue();
     }
 
     @Test
@@ -26,8 +27,8 @@ class AbstractParkingTest {
         Parking parking = new AbstractParking(0, 1);
         Car truck = new Truck(3);
         Car passenger = new Passenger();
-        assertTrue(parking.add(truck));
-        assertFalse(parking.add(passenger));
+        assertThat(parking.add(truck)).isTrue();
+        assertThat(parking.add(passenger)).isFalse();
     }
 
     @Test
@@ -35,26 +36,24 @@ class AbstractParkingTest {
         Parking parking = new AbstractParking(3, 0);
         Car passengerOne = new Passenger();
         Car truck = new Truck(2);
-        parking.add(passengerOne);
-        parking.add(truck);
-        assertEquals(parking.getAllCar().contains(passengerOne), truck);
+        assertThat(parking.add(passengerOne)).isTrue();
+        assertThat(parking.add(truck)).isTrue();
     }
 
     @Test
     public void whenThreePassengerPlaceAndOneTruckPlace() {
-        Parking parking = new AbstractParking(3, 1);
+        Parking parking = new AbstractParking(2, 1);
         Car truckOne = new Truck(2);
         Car truckTwo = new Truck(2);
-        parking.add(truckOne);
-        parking.add(truckTwo);
-        assertEquals(parking.getAllCar().contains(truckOne), truckTwo);
+        assertThat(parking.add(truckOne)).isTrue();
+        assertThat(parking.add(truckTwo)).isTrue();
     }
 
     @Test
     public void whenOnlyTwoPassengerPlace() {
         Parking parking = new AbstractParking(2, 0);
         Car truck = new Truck(3);
-        assertFalse(parking.add(truck));
+        assertThat(parking.add(truck)).isFalse();
     }
 
     @Test
